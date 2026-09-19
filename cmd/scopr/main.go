55203611fc2118c6a4e06b8ecc9c88a210175278
Ananth Narayan <narayanananth26@gmail.com>
2026-09-19 16:06:27 +0530

    feat(cli): parse --where

--- cmd/scopr/main.go
+++ cmd/scopr/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+
+	"scopr/internal/workspace"
+)
+
+// scopr --where
+// Prints .scopr workspace root, returns exit signal
+func runWhere() int {
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "couldn't resolve current working directory: %v\n", err)
+		return 1
+	}
+
+	root, err := workspace.Find(cwd)
+	if errors.Is(err, workspace.ErrNotFound) {
+		fmt.Fprintln(os.Stderr, "not inside a .scopr workspace; run this command from within a workspace")
+		return 1
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return 1
+	}
+
+	fmt.Fprintln(os.Stdout, root)
+	return 0
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	where := flag.Bool("where", false, "print the workspace root and exit")
+	flag.Parse()
+
+	if *where {
+		os.Exit(runWhere())
+	}
+
+	flag.Usage()
+	os.Exit(1)
 }
